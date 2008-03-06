545b74cdfd09b7091ff346cbd70a697b29b01092
mogumbo <mogumbo@23921338-e780-4849-8142-2868373b06eb>
2008-03-06 04:20:22 +0000

    Amended licensing to allow future versions of GPL and LGPL.

--- rssavers/src/util/arguments.cpp
+++ rssavers/src/util/arguments.cpp
@@ -5,7 +5,8 @@
  *
  * rsUtility is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License version 2.1 as published by the Free Software Foundation.
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * rsUtility is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
